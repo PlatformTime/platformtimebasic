3e3b257e294034ba2ef90cad90c892a376784b82
PlatformTime <96312337+PlatformTime@users.noreply.github.com>
2023-01-27 10:29:18 -0500

    Add files via upload

--- LIRR/scripts/bundle.js
+++ LIRR/scripts/bundle.js
@@ -1,3 +1,6 @@
+var globalFeed = [];
+var alertFeed = [];
+
 syntax = "proto2";
 
 import "com/google/transit/realtime/gtfs-realtime.proto";
@@ -58,9 +61,6 @@ message MtaRailroadCarriageDetails {
 extend transit_realtime.VehiclePosition.CarriageDetails {
     optional MtaRailroadCarriageDetails mta_railroad_carriage_details = 1005;
 }
-
-var globalFeed = [];
-var alertFeed = [];
 
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 
